d32ddc589a2e441fd78697564aab1134d835a70d
Michael Duncan <69757294+fugu137@users.noreply.github.com>
2021-02-09 14:06:17 +1100

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,4 @@
-function toggleTutorView() {
-    const sideCol = document.getElementsByClassName("sidecol")[0];
-    const main = document.getElementsByTagName("main")[0];
-
-    if (sideCol.classList.contains("open")) {
-        sideCol.classList.remove("open");
-        main.classList.remove("open");
-
-    } else {
-        sideCol.classList.add("open");
-        main.classList.add("open");
-    }
-}
-
-function toggleMainMenu(tab) {
+function toggleMainTab(tab) {
     var tabs = document.querySelectorAll(".sidebar-nav button");
 
     for (var i = 0; i < tabs.length; i++) {
