fff3fb839def5777dca31d98f096db585b0e877e
Michael Duncan <69757294+fugu137@users.noreply.github.com>
2021-02-15 14:35:43 +1100

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-function toggleMainTab(tab) {
-    var tabs = document.querySelectorAll(".sidebar-nav button");
-
-    for (var i = 0; i < tabs.length; i++) {
-        tabs[i].className = "";
-    }
-    tab.className = "current";
-}
