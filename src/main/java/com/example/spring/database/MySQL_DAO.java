bf546a9958aec13e60f0ebcc3c612b0f8d12991d
Michael Duncan <mjd1702-misc@yahoo.com.au>
2021-05-19 23:45:58 +1000

    Refactored MySQL_DAO class, to improve exception handling and performance related to PreparedStatements.

--- src/main/java/com/example/spring/database/MySQL_DAO.java
+++ src/main/java/com/example/spring/database/MySQL_DAO.java
@@ -8,54 +8,70 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
 
 @Repository("sql_dao")
 public class MySQL_DAO implements DAO {
 
-    private static Connection con = DatabaseConnection.getConnection();
-
+    private static final Connection con = DatabaseConnection.getConnection();
 
     @Override
-    public int insertTutor(Tutor tutor) throws SQLException {
+    public int insertTutor(Tutor tutor) {
         if (tutor == null) {
             return -1;
         }
         tutor.setId();
 
+        int status = 0;
+
         String query = "INSERT INTO tutors(uuid, firstname, lastname, email, imageURL) VALUES (?, ?, ?, ?, ?)";
-        PreparedStatement ps = con.prepareStatement(query);
+        try (PreparedStatement insertTutorPs = con.prepareStatement(query)) {
 
-        ps.setString(1, tutor.getId().toString());
-        ps.setString(2, tutor.getFirstName());
-        ps.setString(3, tutor.getSurname());
-        ps.setString(4, tutor.getEmail());
-        ps.setString(5, tutor.getImagePath());
+            con.setAutoCommit(false);
 
-        List<String> subjects = tutor.getSubjects();
+            insertTutorPs.setString(1, tutor.getId().toString());
+            insertTutorPs.setString(2, tutor.getFirstName());
+            insertTutorPs.setString(3, tutor.getSurname());
+            insertTutorPs.setString(4, tutor.getEmail());
+            insertTutorPs.setString(5, tutor.getImagePath());
 
-        System.out.println(ps);
-        int status = ps.executeUpdate();
+            List<String> subjects = tutor.getSubjects();
 
-        for (String subject : subjects) {
+            System.out.println(insertTutorPs);
+            status = insertTutorPs.executeUpdate();
+
             String query2 = "INSERT INTO tutors_subjects(tutorid, subject) VALUES (?, ?)";
-            PreparedStatement ps2 = con.prepareStatement(query2);
-            ps2.setString(1, tutor.getId().toString());
-            ps2.setString(2, subject);
+            try (PreparedStatement ps2 = con.prepareStatement(query2)) {
 
-            System.out.println(ps2);
-            ps2.executeUpdate();
+                for (String subject : subjects) {
+                    ps2.setString(1, tutor.getId().toString());
+                    ps2.setString(2, subject);
+                    System.out.println(ps2);
+                    ps2.executeUpdate();
+                }
+
+                con.commit();
+                return status;
+
+            } catch (SQLException e) {
+                e.printStackTrace();
+                return -1;
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return -1;
         }
-
-        return status;
     }
 
     @Override
-    public List<Tutor> selectAllTutors(int fromIndex, int numberOfTutors, String[] filters) throws SQLException, IOException {
+    public List<Tutor> selectAllTutors(int fromIndex, int numberOfTutors, String[] filters) {
         int filterCount = filters.length;
 
         String query;
-        PreparedStatement ps;
+        ResultSet rs;
 
         if (filterCount > 0) {
             StringBuilder querySB = new StringBuilder();
@@ -69,67 +85,90 @@ public class MySQL_DAO implements DAO {
             querySB.append(") GROUP BY tutors_subjects.tutorid HAVING COUNT(DISTINCT subject) = ? LIMIT ? OFFSET ?;");
             query = querySB.toString();
 
-            ps = con.prepareStatement(query);
+            try (PreparedStatement filterPs = con.prepareStatement(query)) {
 
-            for (int i = 0; i < filterCount; i++) {
-                ps.setString(i + 1, filters[i]);
+                for (int i = 0; i < filterCount; i++) {
+                    filterPs.setString(i + 1, filters[i]);
+                }
+
+                filterPs.setInt(filterCount + 1, filterCount);
+                filterPs.setInt(filterCount + 2, numberOfTutors);
+                filterPs.setInt(filterCount + 3, fromIndex);
+
+                rs = filterPs.executeQuery();
+                return getSubjectsAndBuildTutorList(rs);
+
+            } catch (SQLException e) {
+                e.printStackTrace();
+                return null;
             }
-
-            ps.setInt(filterCount + 1, filterCount);
-            ps.setInt(filterCount + 2, numberOfTutors);
-            ps.setInt(filterCount + 3, fromIndex);
 
         } else {
             query = "SELECT * FROM tutors LIMIT ? OFFSET ?;";
-            ps = con.prepareStatement(query);
 
-            ps.setInt(1, numberOfTutors);
-            ps.setInt(2, fromIndex);
+            try(PreparedStatement noFilterPs = con.prepareStatement(query)) {
+                noFilterPs.setInt(1, numberOfTutors);
+                noFilterPs.setInt(2, fromIndex);
+
+                rs = noFilterPs.executeQuery();
+                return getSubjectsAndBuildTutorList(rs);
+
+            } catch (SQLException e) {
+                e.printStackTrace();
+                return null;
+            }
         }
-//
-//        System.out.println(query);
-//        System.out.println("PrepState: " + ps);
+    }
 
-        ResultSet rs = ps.executeQuery();
-
+    private List<Tutor> getSubjectsAndBuildTutorList(ResultSet rs) throws SQLException {
         List<Tutor> tutors = new ArrayList<>();
 
         while (rs.next()) {
             List<String> subjects = new ArrayList<>();
 
-            String subjectQuery = "SELECT * FROM tutors_subjects WHERE tutorid = ?";
+            String subjectQuery = "SELECT * FROM tutors_subjects WHERE tutorid = ? GROUP BY subject";
             String idString = rs.getString("uuid");
-            PreparedStatement ps2 = con.prepareStatement(subjectQuery);
-            ps2.setString(1, idString);
-            ResultSet rs2 = ps2.executeQuery();
 
-            while (rs2.next()) {
-                subjects.add(rs2.getString("subject"));
+            try (PreparedStatement ps2 = con.prepareStatement(subjectQuery)) {
+                ps2.setString(1, idString);
+                ResultSet rs2 = ps2.executeQuery();
+
+                while (rs2.next()) {
+                    subjects.add(rs2.getString("subject"));
+                }
+
+                UUID uuid = UUID.fromString(idString);
+                String firstName = rs.getString("firstname");
+                String surname = rs.getString("lastname");
+                String email = rs.getString("email");
+                String imageURL = rs.getString("imageURL");
+
+                Tutor tutor = new Tutor(firstName, surname, email, null, null);
+                tutor.addSubjects(subjects);
+                tutor.setId(uuid);
+                tutor.setImagePath(imageURL);
+
+                tutors.add(tutor);
+
+            } catch (SQLException | IOException e) {
+                e.printStackTrace();
             }
-
-            UUID uuid = UUID.fromString(idString);
-            String firstName = rs.getString("firstname");
-            String surname = rs.getString("lastname");
-            String email = rs.getString("email");
-            String imageURL = rs.getString("imageURL");
-
-            Tutor tutor = new Tutor(firstName, surname, email, null, null);
-            tutor.addSubjects(subjects);
-            tutor.setId(uuid);
-            tutor.setImagePath(imageURL);
-
-            tutors.add(tutor);
         }
 
         return tutors;
     }
 
     @Override
-    public int removeTutor(UUID uid) throws SQLException {
+    public int removeTutor(UUID uid) {
         String query = "DELETE FROM tutors WHERE uuid = ?";
-        PreparedStatement ps = con.prepareStatement(query);
-        ps.setString(1, uid.toString());
 
-        return ps.executeUpdate();
+        try (PreparedStatement ps = con.prepareStatement(query)) {
+            ps.setString(1, uid.toString());
+            return ps.executeUpdate();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            return 0;
+        }
     }
 }
